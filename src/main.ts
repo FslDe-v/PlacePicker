14efe43b2256f28327f057c6cade677b278d9e0d
“Faisal <faisalsamialshalan@gmail.com>
2025-01-19 07:07:54 +0300

    Updated main.ts with new functionality

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
+
+function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  // const request = req.clone({
+  //   headers: req.headers.set('X-DEBUG', 'TESTING'),
+  // });
+  console.log('[Outgoing Request]');
+  console.log(req);
+  return next(req).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
 }).catch((err) => console.error(err));
